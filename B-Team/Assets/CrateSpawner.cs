bf6cc1f7f5a022e85b654b162a31fb65aa934ddc
Mattias Lindblad <panikas94@gmail.com>
2020-01-31 23:07:55 +0100

    good game

---
+++ B-Team/Assets/CrateSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateSpawner : MonoBehaviour
+{
+    public GameObject crate;
+    public GameObject tank;
+    private float timer;
+    public float timeBetweenDrops;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos;
+        newPos = transform.position;
+        newPos.x = tank.transform.position.x + 20;
+        //newPos.y = tank.transform.position.y + 10;
+        transform.position = newPos;
+        if(timer > timeBetweenDrops)
+        {
+            Vector3 v3Rand = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5));
+            Instantiate(crate,transform.position+v3Rand,transform.rotation);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
---
+++ B-Team/Assets/TankController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public Rigidbody rBody;
+    public GameObject barrel;
+    public Transform forcePoint;
+    public float shotForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = barrel.transform.rotation.z;
+        print(f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            //Vector3 v = new Vector3();
+            //print(v);
+            rBody.AddForce(barrel.transform.up*shotForce, ForceMode.Impulse);
+            //rBody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 rot = new Vector3(0, 0, -1);
+            barrel.transform.Rotate(rot);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 rot = new Vector3(0, 0, 1);
+            barrel.transform.Rotate(rot);
+        }
+    }
+}
